0aa2dabdd8958f3f21eddc7b3033f05a2ff61ee9
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-06-20 16:45:42 +0700

    Solved 1

--- Exercise 1: Generate 3 random integers between 100 and 999 which is divisible by 5.py
+++ Exercise 1: Generate 3 random integers between 100 and 999 which is divisible by 5.py
@@ -1,2 +1,10 @@
 # Exercise 1: Generate 3 random integers between 100 and 999 which is divisible by 5
-# debt 1
+
+import random
+
+result = []
+for i in range(3):
+    result.append(random.randrange(100,999,5))
+print(result)
+
+# No need for list
