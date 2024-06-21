c2bf1f019650427bae6fb85a1b13ae8a53e52743
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-06-21 21:15:58 +0700

    Solved 2

--- Exercise 2: Random Lottery Pick. Generate 100 random lottery tickets and pick two lucky tickets from it as a winner.py
+++ Exercise 2: Random Lottery Pick. Generate 100 random lottery tickets and pick two lucky tickets from it as a winner.py
@@ -1,2 +1,11 @@
 # Exercise 2: Random Lottery Pick. Generate 100 random lottery tickets and pick two lucky tickets from it as a winner.
-# debt 2
+
+# you must adhere to the following conditions:
+
+#     The lottery number must be 10 digits long.
+#     All 100 ticket number must be unique.
+import random
+
+lottery_list:list = random.sample(range(1000000000,9999999999),100)
+winning_list:list = random.sample(lottery_list,2)
+print(winning_list)
