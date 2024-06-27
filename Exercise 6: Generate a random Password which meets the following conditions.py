411e3e2798a96a7c0b2d7977289d8d762b76e1e9
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-06-27 20:08:04 +0700

    Added description

--- Exercise 6: Generate a random Password which meets the following conditions.py
+++ Exercise 6: Generate a random Password which meets the following conditions.py
@@ -1,2 +1,6 @@
 # Exercise 6: Generate a random Password which meets the following conditions
-# debt 6
+# Conditions:
+#    Password length must be 10 characters long.
+#    It must contain at least 2 upper case letters, 1 digit, and 1 special symbol.
+
+
