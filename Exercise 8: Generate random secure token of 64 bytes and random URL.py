2e1a55847bf38771135005357363854fe18db821
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-01 21:26:42 +0700

    Solved 8

--- Exercise 8: Generate random secure token of 64 bytes and random URL.py
+++ Exercise 8: Generate random secure token of 64 bytes and random URL.py
@@ -1,2 +1,8 @@
 # Exercise 8: Generate random secure token of 64 bytes and random URL
-# debt 8
+
+
+
+import secrets
+# token_hex vs token_bytes???
+print(f"The secret token is {secrets.token_bytes(64)}")
+print(f"The secret URL is {secrets.token_urlsafe(64)}")
