8c673b7952d790dcef71432f9013bb5108034f16
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-01 21:21:13 +0700

    Solved 7

--- Exercise 7: Calculate multiplication of two random float numbers.py
+++ Exercise 7: Calculate multiplication of two random float numbers.py
@@ -1,2 +1,16 @@
 # Exercise 7: Calculate multiplication of two random float numbers
-# debt  7 + another+another
+# Notes:
+#     First random float number must be between 0.1 and 1
+#     Second random float number must be between 9.5 and 99.5
+
+
+import random 
+# Assign 2 float variables
+num1:float = random.random()
+num2:float = random.uniform(9.5,99.5)
+
+print(f"1st float number is {num1}")
+print(f"2nd float number is {num2}")
+
+result:float = num1*num2
+print(f"The result is {result}")
