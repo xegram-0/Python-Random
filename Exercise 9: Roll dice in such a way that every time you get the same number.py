a7838b1eb1bab3903d4443c52cb78810c1f267ff
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-01 21:39:52 +0700

    Solved 9

--- Exercise 9: Roll dice in such a way that every time you get the same number.py
+++ Exercise 9: Roll dice in such a way that every time you get the same number.py
@@ -1,2 +1,10 @@
 # Exercise 9: Roll dice in such a way that every time you get the same number
-#debt 9
+# I guess seed means key
+# Each time change the seed value,
+# The result will change
+import random
+
+dice:list = [1,2,3,4,5,6]
+for i in range(5):
+    random.seed(53)
+    print(random.choice(dice))
